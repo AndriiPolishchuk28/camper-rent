9d3076e26d9123d7e0dadfafccb7570102b6f60e
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-05-02 01:04:52 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-const test = import.meta.env.VITE_API_TEST;
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Catalog from './pages/Catalog/Catalog';
+import Favourites from './pages/Favourites/Favourites';
+import SharedLayout from './components/SharedLayout/SharedLayout';
 
-function App() {
-  console.log(test);
-  return <div>app</div>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Route>
+    </Routes>
+  );
+};
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,23 @@
-export const Header = () => {
-  return <div>Header</div>;
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/catalog">Catalog</NavLink>
+          </li>
+          <li>
+            <NavLink to="/favourites">Favourites</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
+
+export default Header;
